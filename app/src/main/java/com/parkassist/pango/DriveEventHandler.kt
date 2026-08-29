d19e2e803fb01a708de7bc2c61d3c4a8c623305a
david1966-byte <david1966@gmail.com>
2026-08-29 17:36:18 +0300

    Create DriveEventHandler.kt

---
+++ app/src/main/java/com/parkassist/pango/DriveEventHandler.kt
@@ -0,0 +1,14 @@
+package com.parkassist.pango
+
+import android.content.Context
+
+object DriveEventHandler {
+    fun onDriveStarted(context: Context, repository: ParkingRepository) {
+        if (repository.isCurrentlyDriving()) return
+
+        repository.setCurrentlyDriving(true)
+        FastDriveDetector.stop(context)
+        NotificationHelper.showDriveStartedAlert(context)
+        TtsHelper.speak(context, "התחלת נסיעה. אל תשכח לסיים את החניה באפליקציית פנגו")
+    }
+}
